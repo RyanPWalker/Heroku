bd2aa9824e0cdb40cc28e7a450effa8ce1a8d9e2
Ryan Walker <RyanPrestonWalker@gmail.com>
2017-10-14 23:55:57 -0600

    Fixed session

--- web/sciptures.php
+++ web/sciptures.php
@@ -2,7 +2,6 @@
 	session_start();
 	$db = NULL;
 	try {
-		// default Heroku Postgres configuration URL
 		$dbUrl = getenv('DATABASE_URL');
 		$dbopts = parse_url($dbUrl);
 		$dbHost = $dbopts["host"];
@@ -10,14 +9,10 @@
 		$dbUser = $dbopts["user"];
 		$dbPassword = $dbopts["pass"];
 		$dbName = ltrim($dbopts["path"],'/');
-		// Create the PDO connection
 		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
 		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 	}
 	catch (PDOException $ex) {
-		// If this were in production, you would not want to echo
-		// the details of the exception.
 		echo "Error connecting to DB. Details: $ex";
 		die();
 	}
