b677457edbacd3625bc89163b3749b744d76a53a
Ryan Walker <RyanPrestonWalker@gmail.com>
2017-10-15 22:54:50 -0600

    Editted link page

--- web/Assignments/scriptures.php
+++ web/Assignments/scriptures.php
@@ -33,6 +33,13 @@
 			width: 100%;
 		}
 	</style>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
 	<h1>Here are the database results:</h1>
