982f2f885a3b819e3706419d0caf029f75845726
Ryan Walker <RyanPrestonWalker@gmail.com>
2017-10-21 22:01:51 -0600

    oops

--- web/Assignments/Pear-up/createProfile.php
+++ web/Assignments/Pear-up/createProfile.php
@@ -50,7 +50,8 @@
 															<?php
 																$count = 1;
 																for ($x = 0; $x <= 100; $x++) {
-															   echo '<option value="' . $count . '">' . $count . '</option>';
+															  	echo '<option value="' . $count . '">' . $count . '</option>';
+															  	$count++;
 																}
 															?>
 														</select>
