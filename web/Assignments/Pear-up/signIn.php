586f6396674e21b25f7f39a2f7b07865a2fa72c7
Ryan Walker <RyanPrestonWalker@gmail.com>
2017-10-22 23:11:11 -0600

    debugging

--- web/Assignments/Pear-up/signIn.php
+++ web/Assignments/Pear-up/signIn.php
@@ -7,8 +7,21 @@
 	// Temporary until I add a password hash
 	$_SESSION['online'] = true;
 
-	require("dbConnect.php");
-	$db = get_db();
+	try {
+			$dbUrl = getenv('DATABASE_URL');
+			$dbopts = parse_url($dbUrl);
+			$dbHost = $dbopts["host"];
+			$dbPort = $dbopts["port"];
+			$dbUser = $dbopts["user"];
+			$dbPassword = $dbopts["pass"];
+			$dbName = ltrim($dbopts["path"],'/');
+			$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+			$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+		}
+		catch (PDOException $ex) {
+			echo "Error connecting to DB. Details: $ex";
+			die();
+		}
 
 	$name = $_POST[name];
 	$_SESSION['name'] = $name;
