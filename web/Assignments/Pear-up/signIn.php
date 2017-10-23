8321f38d7bb6838f686d2909d9fe77f1db16f2a5
Ryan Walker <RyanPrestonWalker@gmail.com>
2017-10-22 22:20:53 -0600

    Fixed php

--- web/Assignments/Pear-up/signIn.php
+++ web/Assignments/Pear-up/signIn.php
@@ -14,12 +14,12 @@
 	$statement = $db->prepare($query);
 	$statement->bindValue(':name', $name);
 	$statement->execute();
-	$foundName = $statement->fetch(PDO::FETCH_ASSOC);
+	/*$foundName = $statement->fetch(PDO::FETCH_ASSOC);
 	if ($foundName['name'] != NULL) {
 		/* Redirect browser */
-		header("Location: ./index.php");
-		exit();
-	}
+		//header("Location: ./index.php");
+		//exit();
+	//}
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -48,6 +48,11 @@
 											<div class="6u 12u(xsmall)"><input type="text" name="name" id="name" placeholder="Name" autofocus /></div>
 											<div class="6u 12u(xsmall)"><input type="text" name="password" id="password" placeholder="Password" /></div>
 										</div>
+										<?php
+											//if (($foundName['name'] == NULL) && ($name != NULL)) {
+												//echo '<strong style="color:red">Username not found.</strong>';
+											//}
+										?>
 										<div class="row uniform">
 											<div class="12u">
 												<ul class="actions">
